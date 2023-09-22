5432effc4b5b4f2fe08dd3c50539e4f85d155820
xion93 <dani-deoz@hotmail.com>
2023-09-21 17:48:31 -0700

    First if exercise was added

--- conditionals/if_else_elif.py
+++ conditionals/if_else_elif.py
@@ -21,18 +21,34 @@ def example_two():
 
 
 def example_tree():
-    is_raining_today = True
+    is_raining = False
     is_cold = False
 
     print("Good morning")
-    if is_raining_today and is_cold:
+    if is_raining and is_cold:
         print("Bring Umbrella and jacket")
-    elif is_raining_today and not is_cold:  # this is similar to else and if # similar to ! = Niega la variable
+    elif is_raining and not is_cold:  # this is similar to else and if # similar to ! = Niega la variable
         print("Bring Umbrella")
+    elif not is_raining and is_cold:
+        print("Bring Jacket")
     else:
-        print("Umbrella is Optional")
+        print("Shirt is Fine")
+
+
+def exercise_one():
+    ''' you're doing a credit card purchase
+    all purchases under 50 $ does not need a pin code to be approve
+    '''
+    
+    amount = 51
+    amount_free_to_pin = 50
+    if amount <= amount_free_to_pin:
+        print(f"Purchase of {amount} does not require nip")
+    else:
+        print("Please enter you pin!")
 
 
 # example_one()
 # example_two()
-example_tree()
+# example_tree()
+exercise_one()

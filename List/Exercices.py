3915e49a1aa9aa3dd7e6514c67da36f3ac357e7e
xion93 <dani-deoz@hotmail.com>
2023-10-01 13:00:46 -0700

    Extra info in python list were added

--- List/Exercices.py
+++ List/Exercices.py
@@ -2,6 +2,15 @@
 """
 List can contain multiples values,
 numbers, strings, booleans.
+
+insert i e: Insert integer  at position .
+print: Print the list.
+remove e: Delete the first occurrence of integer .
+append e: Insert integer  at the end of the list.
+sort: Sort the list.
+pop: Pop the last element from the list.
+reverse: Reverse the list.
+
 """
 
 def example_one():

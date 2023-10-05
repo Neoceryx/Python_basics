9aa15e048c6cb2832ebaeb4fa154f1fff3d238f7
xion93 <dani-deoz@hotmail.com>
2023-10-04 20:30:14 -0700

    correct responses were added

--- Projects/Math_Tutor.py
+++ Projects/Math_Tutor.py
@@ -12,7 +12,7 @@ Create application to practice multiplications tables
 '''
 
 
-def count_answers(answers ,no_exercises):
+def count_answers(answers ,no_exercises, responses):
 
     god_answered = answers.count(True)
     correct_percentage = (god_answered / no_exercises) * 100
@@ -21,24 +21,33 @@ def count_answers(answers ,no_exercises):
     print(f"Number of correct answered: {god_answered} / total answers: {no_exercises}")
     print(f"{round(correct_percentage, 2)} % correct ")
 
+    for r in responses:
+        print(r)
+
 
 def generate_questions(num_exercices):
 
     numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     user_answers = list()
+    answers = list()
 
     for i in range(1, num_exercices + 1):
         n1, n2 = r.sample(numbers, 2)  # get two randon number
-        answer = int(input(f"Solve next exersice {n1} x {n2} = ??"))
+        user_answer = int(input(f"Solve next exersice {n1} x {n2} = ??"))
 
         result = n1 * n2
-        if answer == result:
+        answers.append(f"{n1} X {n2} = {result} : you {user_answer}")
+        if user_answer == result:
             user_answers.append(True)
         else:
             user_answers.append(False)
+
+
+
     else:
-        count_answers(user_answers, num_exercices)
+        count_answers(user_answers, num_exercices, answers)
+
 
     pass
     # end function
